20c8291cbb562b9346ca67724e6a03af4745d643
ishwor91 <ishwor.codewing@gmail.com>
2022-08-15 17:57:57 +0545

    wellness health home page finished

--- script/main.js
+++ script/main.js
@@ -6,42 +6,52 @@ const mobile__navigation = document.querySelector(".mobile-menu");
 const mobile__menu = document.querySelector(".mid__nav-container");
 
 menu.addEventListener("click", () => {
-    sec_menu.classList.toggle("active");
-    mobile__navigation.classList.toggle("active");
-    mobile__menu.classList.toggle("active");
-
-
-})
+  sec_menu.classList.toggle("active");
+  mobile__navigation.classList.toggle("active");
+  mobile__menu.classList.toggle("active");
+});
 mobile__navigation.addEventListener("click", () => {
-    sec_menu.classList.toggle("active");
-    menu.classList.toggle("active");
-    main__navigation.classList.toggle("active");
-    mobile__menu.classList.toggle("active");
-
-
-
-    
-})
+  sec_menu.classList.toggle("active");
+  menu.classList.toggle("active");
+  main__navigation.classList.toggle("active");
+  mobile__menu.classList.toggle("active");
+});
 close.addEventListener("click", () => {
-    sec_menu.classList.remove("active");
-    main__navigation.classList.remove("active");
-    mobile__menu.classList.remove("active");
-
-
+  sec_menu.classList.remove("active");
+  main__navigation.classList.remove("active");
+  mobile__menu.classList.remove("active");
 });
 
-
-
-// js for sticky navbar 
+// js for sticky navbar
 let nav = document.querySelector(".bottom__nav");
+
 let sticky = nav.offsetTop;
 myFunc = () => {
   if (window.scrollY >= sticky) {
     nav.classList.add("sticky");
   } else {
     nav.classList.remove("sticky");
-    }
-    
-}
+  }
+};
 window.addEventListener("scroll", myFunc);
 
+
+
+let body = document.body;
+let lastScroll = nav.offsetTop;
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+  if (currentScroll <= 100 ) {
+    body.classList.remove("scrollUp");
+  }
+  if (currentScroll > lastScroll && !body.classList.contains("scrollDown")) {
+    body.classList.add("scrollDown");
+    body.classList.remove("scrollUp");
+  }
+  if (currentScroll < lastScroll && body.classList.contains("scrollDown")) {
+    body.classList.remove("scrollDown");
+    body.classList.add("scrollUp");
+  }
+ lastScroll = currentScroll;
+});
